7ec4e167efd80c83302f752f6949ad3464e393ba
dmitry <dik100695@gmail.com>
2022-09-10 08:07:14 +0300

    translatorFinish

---
+++ script.js
@@ -0,0 +1,10 @@
+"ues script";
+document.getElementById('output').style.visibility = 'hidden';
+document.getElementById('kmInput').addEventListener('input', function (e) {
+	document.getElementById('output').style.visibility = 'visible';
+	let km = e.target.value;
+	document.getElementById('metresOutput').innerHTML = km * 1000;
+	document.getElementById('smOutput').innerHTML = km * 100000;
+	document.getElementById('mlOutput').innerHTML = km * 1000000000;
+
+});
